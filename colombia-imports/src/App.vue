5df1ac2f0593b117369d6bf2fc3d926075e6df1d
edahg <edahg.g@gmail.com>
2021-02-09 23:55:10 -0500

    fixed

--- colombia-imports/src/App.vue
+++ colombia-imports/src/App.vue
@@ -86,7 +86,7 @@ export default {
   data: () => ({
     links: [
       ["Consultar Importaciones", "/"],
-      ["Consultas Guardadas", "/about"],
+      ["Consultas Guardadas", "/savedqueries"],
       ["Lista Proveedores", "/providers"],
     ],
     user: { 
